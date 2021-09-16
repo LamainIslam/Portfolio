d72de75d36b57849506304b78ce8b9a0422a4d8d
LamainIslam <Lamainislam@gmail.com>
2021-09-16 23:58:34 +0100

    colour and hero layout

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const menuBtn = document.querySelector('.menu-btn');
 const navbar = document.querySelector('.main-nav');
-const navitems = document.querySelector('nav-items');
+const NI1 = document.querySelector('.ni1');
+const NI2 = document.querySelector('.ni2');
+const NI3 = document.querySelector('.ni3');
+const NI4 = document.querySelector('.ni4');
 let menuOpen = false;
 
 menuBtn.addEventListener('click', () => {
@@ -15,7 +18,40 @@ menuBtn.addEventListener('click', () => {
   }
 });
 
-navitems.addEventListener('click', () => {
+NI1.addEventListener('click', () => {
+  if(!menuOpen) {
+    menuBtn.classList.add('open');
+    navbar.classList.add('open');
+    menuOpen = true;
+  } else {
+    menuBtn.classList.remove('open');
+    navbar.classList.remove('open');
+    menuOpen = false;
+  }
+});
+NI2.addEventListener('click', () => {
+  if(!menuOpen) {
+    menuBtn.classList.add('open');
+    navbar.classList.add('open');
+    menuOpen = true;
+  } else {
+    menuBtn.classList.remove('open');
+    navbar.classList.remove('open');
+    menuOpen = false;
+  }
+});
+NI3.addEventListener('click', () => {
+  if(!menuOpen) {
+    menuBtn.classList.add('open');
+    navbar.classList.add('open');
+    menuOpen = true;
+  } else {
+    menuBtn.classList.remove('open');
+    navbar.classList.remove('open');
+    menuOpen = false;
+  }
+});
+NI4.addEventListener('click', () => {
   if(!menuOpen) {
     menuBtn.classList.add('open');
     navbar.classList.add('open');
